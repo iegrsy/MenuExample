450da1724f18f6784f960cd732ca93bc1c85511c
iegrsy <iegursoy28@gmail.com>
2017-08-10 18:00:20 +0300

    first commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,13 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->lstMenu->installEventFilter(this);
+
+    QString path = "/home/ieg/qt/qtProjects/MenuExample/test.json";
+    QString jData = readFile(path);
+
+    readJson(jData);
+    updateListMenu();
 }
 
 MainWindow::~MainWindow()
@@ -13,4 +20,107 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+QString MainWindow::readFile(QString path)
+{
+    QFile file(path);
+    if(!file.open(QIODevice::ReadOnly))
+        return 0;
 
+    QString jdata = file.readAll().data();
+    file.close();
+
+    return jdata;
+}
+
+void MainWindow::readJson(QString str)
+{
+    if(str.isEmpty())
+        return;
+
+    jDoc = QJsonDocument::fromJson(str.toUtf8());
+    currentObj = jDoc.object();
+}
+
+void MainWindow::showObj(QJsonObject obj)
+{
+    ui->lstMenu->clear();
+    foreach (QString s, obj.keys())
+    {
+        if(obj[s].isObject())
+        {
+            QListWidgetItem *item = new QListWidgetItem;
+            item->setIcon(QIcon("/home/ieg/Resimler/icon.png"));
+            item->setText(s);
+
+            ui->lstMenu->addItem(item);
+        }
+        else if(obj[s].isArray())
+        {
+            ui->lstMenu->addItem(s);
+            //            for (int i=0; i<obj[s].toArray().count() ; i++)
+            //            {
+            //                ui->lstMenu->addItem(obj[s].toArray().at(i).toObject()["name"].toString());
+            //            }
+        }
+    }
+    ui->lstMenu->addItem("back_menu");
+    ui->lstMenu->setCurrentRow(0);
+}
+
+void MainWindow::updateListMenu()
+{
+    showObj(currentObj);
+}
+
+void MainWindow::intoObject(QJsonObject obj){
+
+    prevObj.append(currentObj);
+
+    currentObj = obj;
+    updateListMenu();
+}
+
+void MainWindow::backMenu()
+{
+    if(!prevObj.isEmpty()){
+        currentObj = prevObj.last();
+        showObj(currentObj);
+        prevObj.removeLast();
+    }
+}
+
+bool MainWindow::eventFilter(QObject *obj, QEvent *event)
+{
+    if (obj == ui->lstMenu)
+    {
+        QListWidget *lstMenu = static_cast<QListWidget*>(obj);
+
+        //Capture key event
+        if (event->type() == QEvent::KeyPress)
+        {
+            QKeyEvent *keyEvent = static_cast<QKeyEvent*>(event);
+            if(keyEvent->key() == Qt::Key_Return)
+            {
+                QString selectItem = lstMenu->selectedItems().first()->text();
+                if(currentObj[selectItem].isObject())
+                {
+                    intoObject(currentObj[selectItem].toObject());
+                }
+                else if (currentObj[selectItem].isArray())
+                {
+                    qDebug()<<selectItem;
+                }
+               if (selectItem.compare("back_menu") == 0)
+                {
+                    backMenu();
+                    qDebug()<<selectItem;
+                }
+            }
+            return false;
+        }
+    }
+    else
+    {
+        return QMainWindow::eventFilter(obj, event);
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QFile>
+#include <QDebug>
+#include <QStringList>
+#include <QListWidgetItem>
+
+#include <QJsonParseError>
+#include <QDragEnterEvent>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +27,18 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    QString readFile(QString path);
+    void readJson(QString str);
+    void showObj(QJsonObject obj);
+    void updateListMenu();
+    bool eventFilter(QObject *obj, QEvent *event);
+    void intoObject(QJsonObject obj);
+    void backMenu();
 private:
     Ui::MainWindow *ui;
+    QJsonDocument jDoc;
+    QJsonObject currentObj;
+    QList<QJsonObject> prevObj;
 };
 
 #endif // MAINWINDOW_H
